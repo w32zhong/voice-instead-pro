4cbfc50fb04e8d21f5edb38c0657efc98c8f633b
t-k- <clock126@126.com>
2017-12-16 02:15:08 -0500

    Add Baidu Deep Voice

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 function config_default() {
 	return {
-	'modifyDate': 'Sat Dec 16 00:54:45 EST 2017',
+	'modifyDate': 'Sat Dec 16 02:14:34 EST 2017',
 	'apiChoice': 0,
 	'popup_playing': false,
 	'apiList':
@@ -158,83 +158,110 @@ function config_default() {
 				}
 			]
 		},
-//		{
-//			'name': 'Baidu Deep Speech',
-//			'website': 'http://yuyin.baidu.com/#try',
-//			'url': 'https://ai.baidu.com/aidemo?type=tns2&idx=1&tex=hello%2520world&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=2&vol=5&pit=5',
-//			'txt_uri_key': 't',
-//			'voice_gap': 0.55,
-//			'options': [
-//				{
-//					'name': 'Speech speed',
-//					'uri_key': 'rate',
-//					'choice': '0.5',
-//					'uri_val': [
-//						['Very slow', '0.2'],
-//						['Slow', '0.3'],
-//						['Normal', '0.5'],
-//						['Fast', '0.7'],
-//						['Very Fast', '0.8'],
-//						['Bolt Speech', '1.0']
-//					]
-//				},
-//				{
-//					'name': 'Volume',
-//					'uri_key': 'vol',
-//					'choice': '1.0',
-//					'uri_val': [
-//						['1.0', '1.0'],
-//						['0.8', '0.8'],
-//						['0.6', '0.6'],
-//						['0.4', '0.4'],
-//						['0.2', '0.2']
-//					]
-//				},
-//				{
-//					'name': 'Language',
-//					'uri_key': 'tl',
-//					'choice': 'en_US',
-//					'uri_val': [
-//						['English United States', 'en_US'],
-//						['English United Kingdom', 'en_GB'],
-//						['English India', 'en_IN'],
-//						['German Germany', 'de_DE'],
-//						['Spanish Spain', 'es_ES'],
-//						['Spanish Mexico', 'es_MX'],
-//						['Spanish United States', 'es_US'],
-//						['French Belgium', 'fr_BE'],
-//						['French France', 'fr_FR'],
-//						['Indonesian Indonesia', 'in_ID'],
-//						['Italian Italy', 'it_IT'],
-//						['Hindi India', 'hi_IN'],
-//						['Japanese Japan', 'ja_JP'],
-//						['Korean South Korea', 'ko_KR'],
-//						['Dutch Netherlands', 'nl_NL'],
-//						['Polish Poland', 'pl_PL'],
-//						['Portuguese Brazil', 'pt_BR'],
-//						['Portuguese Portugal', 'pt_PT'],
-//						['Russian Russia', 'ru_RU'],
-//						['Thai Thailand', 'th_TH'],
-//						['Turkish Turkey', 'tr_TR'],
-//						['Chinese China', 'zh_CN'],
-//						['Chinese Hong Kong', 'zh_HK'],
-//						['Chinese Taiwan', 'zh_TW']
-//					]
-//				},
-//				{
-//					'name': 'Pitch',
-//					'uri_key': 'pitch',
-//					'choice': '0.5',
-//					'uri_val': [
-//						['0.1', '0.1'],
-//						['0.3', '0.3'],
-//						['0.5', '0.5'],
-//						['0.7', '0.7'],
-//						['0.9', '0.9']
-//					]
-//				}
-//			]
-//		},
+		{
+			'name': 'Baidu Deep Voice',
+			'website': 'http://yuyin.baidu.com/#try',
+			//'url': 'https://ai.baidu.com/aidemo',
+			'url': 'http://tsn.baidu.com/text2audio',
+			'txt_uri_key': 'tex',
+			'voice_gap': 0,
+			'options': [
+				{
+					'name': '_',
+					'uri_key': 'type',
+					'choice': 'tns',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'idx',
+					'choice': '1',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'cuid',
+					'choice': 'baidu_speech_demo',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'cod',
+					'choice': '2',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'lan',
+					'choice': 'zh',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'ctp',
+					'choice': '1',
+					'uri_val': []
+				},
+				{
+					'name': '_',
+					'uri_key': 'pdt',
+					'choice': '1',
+					'uri_val': []
+				},
+				{
+					'name': 'Voice',
+					'uri_key': 'per',
+					'choice': '1',
+					'uri_val': [
+						['Woman', '0'],
+						['Man', '1']
+					]
+				},
+				{
+					'name': 'Volume',
+					'uri_key': 'vol',
+					'choice': '5',
+					'uri_val': [
+						['2', '2'],
+						['3', '3'],
+						['5', '5'],
+						['7', '7'],
+						['9', '9'],
+						['11', '11'],
+						['13', '13'],
+						['15', '15']
+					]
+				},
+				{
+					'name': 'Pitch',
+					'uri_key': 'pit',
+					'choice': '5',
+					'uri_val': [
+						['0', '0'],
+						['1', '1'],
+						['3', '3'],
+						['5', '5'],
+						['7', '7'],
+						['8', '8'],
+						['9', '9']
+					]
+				},
+				{
+					'name': 'Speed',
+					'uri_key': 'spd',
+					'choice': '5',
+					'uri_val': [
+						['0', '0'],
+						['1', '1'],
+						['3', '3'],
+						['5', '5'],
+						['7', '7'],
+						['8', '8'],
+						['9', '9']
+					]
+				}
+			]
+		},
 		{
 			'name': 'Voice RSS',
 			'website': 'http://www.voicerss.org/api/',
